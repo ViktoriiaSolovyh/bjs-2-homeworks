e6f5307b23505c12d88dee66064447cc7fafaac6
Viktorria <vika.solovykh.91@bk.ru>
2023-08-30 12:36:12 +0300

    выполнено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -10,19 +10,19 @@ Student.prototype.setSubject = function(subjectName) {
 }
 
 Student.prototype.addMarks = function(...marks) {
-    if (this.marks) {
-      this.marks.push(...marks);
-    }
-    }
-    Student.prototype.getAverage = function() {
-      if (this.marks) {
-        return this.marks.reduce((averageMark, mark) => averageMark + mark / this.marks.length, 0);
-      }
-      return 0;
-    }
+  if (this.marks) {
+    this.marks.push(...marks);
+  }
+}
+Student.prototype.getAverage = function() {
+  if (this.marks) {
+    return this.marks.reduce((averageMark, mark) => averageMark + mark / this.marks.length, 0);
+  }
+  return 0;
+}
 
-    Student.prototype.exclude = function(reason) {
-      delete this.subject;
-      delete this.marks;
-      this.excluded = reason;
-    }
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
