9a3e48555b1bd9829866635cd84c0d259d56b3bc
Viktorria <vika.solovykh.91@bk.ru>
2023-09-02 16:18:00 +0300

    выполнено

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,56 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+	let cache = new Map();
+	let arr = [];
+	let count = 0;
+
+	return function(...args) {
+		const hash = [...args];
+
+		if (hash in cache) {
+			const index = arr.indexOf(hash);
+			arr.splice(index, 1);
+			arr.push(hash);
+			return "Из кеша: " +
+				cache[hash];
+		}
+
+		const result = func(...args);
+		cache[hash] = result;
+		arr.push(hash);
+		count++;
+
+		if (count > 5) {
+			const oldHash = arr.shift();
+			delete cache[oldHash];
+			count--;
+		}
+
+		return "Вычисляем: " + result;
+	};
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+	let timeoutId;
+	let isFirstCall = false;
+
+	function wrapper(...args) {
+		clearTimeout(timeoutId);
+		timeoutId = setTimeout(() => {
+			func(args);
+			wrapper.count++;
+		}, delay);
+
+		if (!isFirstCall) {
+			func(...args);
+			wrapper.count++;
+			isFirstCall = true;
+		}
+		wrapper.allCount++;
+	}
+
+	wrapper.count = 0;
+	wrapper.allCount = 0;
+	return wrapper;
 }
