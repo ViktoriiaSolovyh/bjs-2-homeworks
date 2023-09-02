d300d402988dfc6b1be96aaabdd170cef54639ca
Viktorria <vika.solovykh.91@bk.ru>
2023-09-02 15:55:44 +0300

    выполнено

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿function parseCount(value) {
+	if (Number.isNaN(Number.parseFloat(value))) {
+		throw new Error("Невалидное значение");
+	}
+
+	return Number.parseFloat(value);
+}
+
+function validateCount(count) {
+	try {
+		return parseCount(count);
+	} catch (error) {
+		return error;
+	}
+}
+
+class Triangle {
+	constructor(a, b, c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+
+		if (this.a > this.b + this.c || this.b > this.a + this.c || this.c > this.a + this.b) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		}
+	}
+
+	get perimeter() {
+		return this.a + this.b + this.c;
+	}
+
+	get area() {
+		const p = this.perimeter / 2;
+		return +Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3);
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c);
+	} catch (error) {
+		return {
+			get perimeter() {
+				return "Ошибка! Треугольник не существует";
+			},
+			get area() {
+				return "Ошибка! Треугольник не существует";
+			},
+		};
+	}
+}
